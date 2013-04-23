10193b76006fec7ddc5673a6a34905ab794f8d01
Liraz Siri <liraz@turnkeylinux.org>
2013-04-24 01:40:52 +0300

    update_shortlinks.php: added DROP/CREATE option

--- htdocs/update_shortlinks.php
+++ htdocs/update_shortlinks.php
@@ -10,32 +10,70 @@ function fatal($s) {
 
 function usage() {
     echo<<<"EOD"
-Syntax: {$GLOBALS['argv'][0]} ( --tx | --address )
+Syntax: {$GLOBALS['argv'][0]} [ -c ] ( -t | -a )
+
+Options:
+    -t      Create tx shortlinks
+    -a      Create address shortlinks
+
+    -c      Create DROP/CREATE SQL
 
 EOD;
     exit(1);
 }
 
-$args = $GLOBALS['argv'];
-if(count($args) != 2 || !preg_match('/^--(tx|address)/', $args[1], $preg_m))
-    usage();
+function main() {
 
-$mode = $preg_m[1];
+    $opts = getopt("cath");
 
-$fh = fopen("php://stdin","r");
-while($line = trim(fgets($fh))) {
-    $arr = explode(" ", $line);
-    if($mode == "tx") {
-        $shortcut_hex = decodeBase58($arr[0]);
-        $tx_hex = $arr[1];
+    if(isset($opts['h']))
+        usage();
 
-        echo "INSERT INTO t_shortlinks(shortcut, hash) VALUES (decode('$shortcut_hex', 'hex'), decode('$tx_hex', 'hex'));\n";
-    } elseif($mode == "address") {
-        
-        $shortcut_hex = decodeBase58($arr[0]);
-        $hash160_hex = addressToHash160($arr[1]);
+    if(isset($opts['a']) && isset($opts['b'])) 
+        fatal("can't use -a and -t together");
 
-        echo "INSERT INTO a_shortlinks(shortcut, hash160) VALUES (decode('$shortcut_hex', 'hex'), decode('$hash160_hex', 'hex'));\n";
-        
+    $mode = isset($opts['t']) ? 'tx' : 'address';
+
+    if(isset($opts['c'])) {
+        echo <<< 'EOD'
+
+DROP TABLE t_shortlinks;
+CREATE TABLE t_shortlinks (
+    shortcut bytea NOT NULL PRIMARY KEY,
+    hash bytea NOT NULL REFERENCES transactions
+);
+
+ALTER TABLE public.t_shortlinks OWNER TO blockupdate;
+
+DROP TABLE a_shortlinks;
+CREATE TABLE a_shortlinks (
+    shortcut bytea NOT NULL PRIMARY KEY,
+    hash160 bytea NOT NULL REFERENCES keys
+);
+
+ALTER TABLE public.a_shortlinks OWNER TO blockupdate;
+
+
+EOD;
+
+    }
+    $fh = fopen("php://stdin","r");
+    while($line = trim(fgets($fh))) {
+        $arr = explode(" ", $line);
+        if($mode == "tx") {
+            $shortcut_hex = decodeBase58($arr[0]);
+            $tx_hex = $arr[1];
+
+            echo "INSERT INTO t_shortlinks(shortcut, hash) VALUES (decode('$shortcut_hex', 'hex'), decode('$tx_hex', 'hex'));\n";
+        } elseif($mode == "address") {
+            
+            $shortcut_hex = decodeBase58($arr[0]);
+            $hash160_hex = addressToHash160($arr[1]);
+
+            echo "INSERT INTO a_shortlinks(shortcut, hash160) VALUES (decode('$shortcut_hex', 'hex'), decode('$hash160_hex', 'hex'));\n";
+            
+        }
     }
 }
+
+main();
